a848876be1e95ef71bb2f3efb56c82825cb1b688
Adrian Hazzard <adrian.hazzard@nottingham.ac.uk>
2017-05-03 10:57:19 +0100

    content updates from adrian

--- scripts/runmeld.sh
+++ scripts/runmeld.sh
@@ -6,7 +6,7 @@ echo "IP = " $IP
 export MELD_BASE_URI=http://${IP}:5000
 export MELD_MEI_URI=http://${IP}:3000/content
 export MELD_MUZICODES_URI=http://${IP}:3000
-export MELD_BASECAMP_MEI_FILE="http://${IP}:3000/content/MSThe%20Climb%20(Base%20Camp).mei"
+export MELD_BASECAMP_MEI_FILE="http://${IP}:3000/content/BaseCamp.mei"
 
 echo "Open " ${MELD_BASE_URI}"/startTheClimb"
 cd meld
